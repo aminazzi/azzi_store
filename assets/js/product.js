fbb9361998988785166fc2dd6c2805c1ab0a8857
aminazzi <gjw511914@gmail.com>
2026-08-03 11:24:04 +0100

    Add buy button redirect to payment link

--- assets/js/product.js
+++ assets/js/product.js
@@ -16,5 +16,6 @@ fetch("../assets/data/models.json")
     document.getElementById("product-description").textContent = product.description;
     document.getElementById("product-format").textContent = product.format;
     document.getElementById("product-size").textContent = product.size;
-
+    document.getElementById("buy-btn").addEventListener("click", () => {
+    window.location.href = "https://payhip.com/b/8g0vs";
   });
