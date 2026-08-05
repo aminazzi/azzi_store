34b7163d4e706c1bb6b00f7f44b72ebd324534da
aminazzi <gjw511914@gmail.com>
2026-08-05 17:02:36 +0100

    Add console log for product ID from URL
    Log the product ID retrieved from URL parameters.

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,5 +1,6 @@
 const params = new URLSearchParams(window.location.search);
-const id = Number(params.get("id"));
+
+console.log(params.get("id"));
 
 Promise.all([
   fetch("../assets/data/models.json").then(r => r.json()),
