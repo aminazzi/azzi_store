b2da111abb512793229359ae3e860c298e93359d
aminazzi <gjw511914@gmail.com>
2026-08-24 12:31:14 +0100

    Fix formatting of product link assignment

--- assets/js/script.js
+++ assets/js/script.js
@@ -105,7 +105,7 @@ async function searchProducts() {
              * بدل الاعتماد على product.page
              */
             const productLink =
-    `pages/product.html?id=${encodeURIComponent(product.id)}`;
+            `pages/product.html?id=${encodeURIComponent(product.id)}`;
 
 
             /*
